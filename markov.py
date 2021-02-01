7ff9c8f22363a891dbacd8441206ed5befe7bf28
Manikandan Sundararajan <mani@hey.com>
2021-02-01 04:35:53 +0000

    Create a corpus of distinct words and list of tokens

--- markov.py
+++ markov.py
@@ -11,3 +11,14 @@ def process_corpus(corpus):
   for spaced in ['.','-',',','!','?','(','—',')']:
       corpus = corpus.replace(spaced, ' {0} '.format(spaced))
   print(len(corpus))
+
+  corpus_words = corpus.split(' ')
+  corpus_words = [word for word in corpus_words if word != '']
+  print(corpus_words[:10])
+  print(len(corpus_words))
+
+  distinct_words = list(set(corpus_words))
+  word_idx_dict = {word: i for i, word in enumerate(distinct_words)}
+
+  distinct_words_count = len(list(set(corpus_words)))
+  print(distinct_words_count)
